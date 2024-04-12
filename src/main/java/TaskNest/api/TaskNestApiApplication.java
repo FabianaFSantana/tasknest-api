f2bf373d78d70cdb3c2a4925f8db1d16f3348225
Fabiana <fabiana.lua@gmail.com>
2024-04-12 08:27:23 -0300

    Criação do arquivo do projeto

---
+++ src/main/java/TaskNest/api/TaskNestApiApplication.java
@@ -0,0 +1,13 @@
+package TaskNest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TaskNest/api/TaskNestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskNest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
