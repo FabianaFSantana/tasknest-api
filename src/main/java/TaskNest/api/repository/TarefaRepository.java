6fb5c4da5b45b6e8466ee938f81634e94da997c1
Fabiana <fabiana.lua@gmail.com>
2024-04-14 14:15:03 -0300

    TAS-004 Criação da classe TarefaRepository

---
+++ src/main/java/TaskNest/api/repository/TarefaRepository.java
@@ -0,0 +1,11 @@
+package TaskNest.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import TaskNest.api.model.Tarefa;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    
+}
