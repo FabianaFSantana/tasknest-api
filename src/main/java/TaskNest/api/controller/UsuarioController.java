6545b71cbf70c82cbdbeb421f3ab12536603aff5
Fabiana <fabiana.lua@gmail.com>
2024-04-14 08:59:32 -0300

    TAS-002 Criação dos endpoints PUT para atualizar usuario e delete para exluir usuário. Ajuste na classe model Usuario pra inserir atributo senha.

--- src/main/java/TaskNest/api/controller/UsuarioController.java
+++ src/main/java/TaskNest/api/controller/UsuarioController.java
@@ -8,9 +8,11 @@ import javax.print.DocFlavor.STRING;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,7 +61,42 @@ public class UsuarioController {
         .body(usuariorRepository.findById(idUsuario));
     }
 
-    
+    @PutMapping("/{idUsuario}")
+    public ResponseEntity<Usuario> atulizarDadosUsuario(@PathVariable("idUsuario") Long idUsuario,
+    @RequestBody Usuario usuario) {
+
+        Optional<Usuario> usuarOptional = usuariorRepository.findById(idUsuario);
+        if (usuarOptional.isPresent()) {
+            Usuario usuarioEncont = usuarOptional.get();
+
+            usuarioEncont.setNome(usuario.getNome());
+            usuarioEncont.getEndereco().setCep(usuario.getEndereco().getCep());
+            usuarioEncont.getEndereco().setEndereco(usuario.getEndereco().getEndereco());
+            usuarioEncont.getEndereco().setNumero(usuario.getEndereco().getNumero());
+            usuarioEncont.getEndereco().setComplemento(usuario.getEndereco().getComplemento());
+            usuarioEncont.getEndereco().setBairro(usuario.getEndereco().getBairro());
+            usuarioEncont.getEndereco().setCidade(usuario.getEndereco().getCidade());
+            usuarioEncont.getEndereco().setEstado(usuario.getEndereco().getEstado());
+            usuarioEncont.getEndereco().setPais(usuario.getEndereco().getPais());
+            usuarioEncont.setEmail(usuario.getEmail());
+            usuarioEncont.setSenha(usuario.getSenha());
+            usuarioEncont.setTelefone(usuario.getTelefone());
+
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(usuariorRepository.save(usuarioEncont));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<String> excluirUsuario(@PathVariable("idUsuario") Long idUsuario) {
+        usuariorRepository.deleteById(idUsuario);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Usuário excluído com sucesso.");
+    }
+
+
 
 
 
--- src/main/java/TaskNest/api/model/Usuario.java
+++ src/main/java/TaskNest/api/model/Usuario.java
@@ -33,11 +33,29 @@ public class Usuario {
 
     @NotBlank(message = "O email é obrigatório.")
     @Size(max = 50, message = "O email não pode ter mais de 50 caracteres")
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
-
+    
+    @NotBlank(message = "A senha é obrigatória.")
+    @Size(min = 6, max = 8, message = "A senha deve conter entre 6 e 8 caracteres.")
+    @Column(nullable = false)
+    private String senha;
+    
     @NotBlank(message = "O telefone é obrigatório")
     @Size(min = 10, max = 20, message = "O telefone precisa ter entre 10 e 20 caracteres")
     @Pattern(regexp = "\\+?[0-9]+", message = "O telefone deve conter apenas números.")
     private String telefone;
+
+    //Método para validar a senha do usuário:
+    /*public boolean validarSenha(String senha) {
+
+        boolean temComprimento = senha.length() >= 6 && senha.length() <= 8;
+        boolean temCaracterEspecial = senha.matches(".*[!@#$%&*?!].*");
+        boolean temNumero = senha.matches(".*[\\d].*");
+        boolean temLetraMinuscula = senha.matches(".*[a-z].*");
+        boolean temLetraMaiscula = senha.matches(".*[A-Z].*");
+
+        return temComprimento && temCaracterEspecial && temNumero && temLetraMinuscula && temLetraMaiscula;
+
+    }*/
 }
