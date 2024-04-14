86bb75b05fcadc339aa947b883069bcb55250d71
Fabiana <fabiana.lua@gmail.com>
2024-04-14 19:53:19 -0300

    TAS-004 Criação dos endpoints do tipo GET para exibir lista de tarefas e buscar tarefa pelo id, pela data de criação, pela data de vencimento, pela prioridade, pelo status e pelo titulo

--- src/main/java/TaskNest/api/controller/TarefaController.java
+++ src/main/java/TaskNest/api/controller/TarefaController.java
@@ -1,13 +1,21 @@
 package TaskNest.api.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import TaskNest.api.constants.Prioridade;
+import TaskNest.api.constants.Status;
 import TaskNest.api.model.Tarefa;
 import TaskNest.api.repository.TarefaRepository;
 
@@ -24,6 +32,82 @@ public class TarefaController {
         .body(tarefaRepository.save(tarefa));
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<Tarefa>> exibirListaDeTodasAsTarefas() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(tarefaRepository.findAll());
+    }
+
+    @GetMapping("/bucarPeloId/{id}")
+    public ResponseEntity<Optional<Tarefa>> buscarTarefaPeloId(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(tarefaRepository.findById(id));
+    }
+
+    @GetMapping("/buscarPelaDataCriacao/{dataDeCriacao}")
+    public ResponseEntity<List<Tarefa>> buscarTarefaPelaDataDeCriacao(@PathVariable("dataDeCriacao") LocalDate dataDeCriacao) {
+        List<Tarefa> tarefas = tarefaRepository.findAllByDataCriacao(dataDeCriacao);
+
+        if (!tarefas.isEmpty()) {         
+            return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarPelaDataVencimento/{dataDeVencimento}")
+    public ResponseEntity<List<Tarefa>> buscarTarefaPelaDataDeVencimento(@PathVariable("dataDeVencimento") LocalDate dataDeVencimento) {
+        List<Tarefa> tarefas = tarefaRepository.findAllByDataVencimento(dataDeVencimento);
+
+        if (!tarefas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarPelaPrioridade/{prioridade}")
+    public ResponseEntity<List<Tarefa>> buscarTarefaPelaPrioridade(@PathVariable("prioridade") Prioridade prioridade) {
+        List<Tarefa> tarefas = tarefaRepository.findAllByPrioridade(prioridade);
+
+        if (!tarefas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarPeloStatus/{status}")
+    public ResponseEntity<List<Tarefa>> buscarTarefaPeloStatus(@PathVariable("status") Status status) {
+        List<Tarefa> tarefas = tarefaRepository.findAllByStatus(status);
+
+        if (!tarefas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarPeloTitulo/{titulo}")
+    public ResponseEntity<Tarefa> buscarTarefaPeloTitulo(@PathVariable("titulo") String titulo) {
+        Optional<Tarefa> tarefOptional = tarefaRepository.findByTitulo(titulo);
+
+        if (tarefOptional.isPresent()) {
+            Tarefa tarefa = tarefOptional.get();
+            return ResponseEntity.status(HttpStatus.OK).body(tarefa);
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
+
+
 
 }
--- src/main/java/TaskNest/api/repository/TarefaRepository.java
+++ src/main/java/TaskNest/api/repository/TarefaRepository.java
@@ -1,11 +1,33 @@
 package TaskNest.api.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import TaskNest.api.constants.Prioridade;
+import TaskNest.api.constants.Status;
 import TaskNest.api.model.Tarefa;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
     
+    @Query("SELECT t FROM tarefa t WHERE t.dataDeCriacao = :dataDeCriacao")
+    List<Tarefa> findAllByDataCriacao(@Param("dataDeCriacao") LocalDate dataDeCriacao);
+
+    @Query("SELECT v FROM tarefa v WHERE v.dataDeVencimento = :dataDeVencimento")
+    List<Tarefa> findAllByDataVencimento(@Param("dataDeVencimento") LocalDate dataDeVencimento);
+
+    @Query("SELECT p FROM tarefa p WHERE p.prioridade = :prioridade")
+    List<Tarefa> findAllByPrioridade(@Param("prioridade") Prioridade prioridade);
+
+    @Query("SELECT s FROM tarefa s WHERE s.status = :status")
+    List<Tarefa> findAllByStatus(@Param("status") Status status);
+
+    @Query("SELECT t FROM tarefa t WHERE t.titulo = :titulo")
+    Optional<Tarefa> findByTitulo(@Param("titulo") String titulo);
 }
