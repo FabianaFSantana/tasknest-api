905e04a76b11da29ca6043b6564aef3bbc9b5162
Fabiana <fabiana.lua@gmail.com>
2024-04-15 20:56:33 -0300

    TAS-009 Criação do método para enviar notificação da tarefa para o usuário na classe MensagemSmsService.

--- src/main/java/TaskNest/api/service/MensagemSmsService.java
+++ src/main/java/TaskNest/api/service/MensagemSmsService.java
@@ -9,6 +9,9 @@ import com.twilio.rest.accounts.v1.credential.PublicKey;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
+import TaskNest.api.constants.Status;
+import TaskNest.api.model.Tarefa;
+import TaskNest.api.model.Usuario;
 import TaskNest.api.repository.TarefaRepository;
 
 @Service
@@ -36,6 +39,31 @@ public class MensagemSmsService {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void enviarNotificacaoDeTarefaPorSms(Usuario usuario, Tarefa tarefa) {
+
+        if (!tarefa.getStatus().equals(Status.PENDENTE)) {
+            throw new IllegalStateException("Tarefa esprando ser enviada."); 
+        }
+
+        try {
+            String mensagemSms = "Olá, " + usuario.getNome() +"! Você tem uma notificação pendente!\n\n" +
+                                 "Título: " + tarefa.getTitulo() + "\n" +
+                                 "Descrição: " + tarefa.getDescricao() + "\n" +
+                                 "Status: " + tarefa.getStatus() + "\n" +
+                                 "Data de Criação: " + tarefa.getDataDeCriacao() + "\n" + 
+                                 "Prioridade: " + tarefa.getPrioridade() + "\n" +
+                                 "Data de Vencimento: " + tarefa.getDataDeVencimento() + "\n";
+
+            enviarMensagem(usuario.getTelefone(), mensagemSms);
+            tarefaRepository.save(tarefa);
+
+        } catch (Exception e) {
+
+            throw new RuntimeException("Falha ao enviar a notificação.", e);
+
         }
     }
     
