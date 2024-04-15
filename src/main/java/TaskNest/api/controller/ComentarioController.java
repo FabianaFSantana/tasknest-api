54ef7c81d287b4c9becf558ad8a1526b514c1122
Fabiana <fabiana.lua@gmail.com>
2024-04-14 22:08:04 -0300

    TAS-005 Criação dos endpoints PUT e DELETE para atualizar e remover comentário

--- src/main/java/TaskNest/api/controller/ComentarioController.java
+++ src/main/java/TaskNest/api/controller/ComentarioController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +44,34 @@ public class ComentarioController {
         .body(comentarioRepository.findById(idComent));
     }
 
-    
+    @PutMapping("/{idComent}")
+    public ResponseEntity<Comentario> atualizarComentario(@PathVariable("idComent") Long idComent,
+    @RequestBody Comentario comentario) {
+
+        Optional<Comentario> comentOptional = comentarioRepository.findById(idComent);
+        if (comentOptional.isPresent()) {
+            Comentario comentEncont = comentOptional.get();
+
+            comentEncont.setTexto(comentario.getTexto());
+            comentEncont.setDataDeCriacaoComent(comentario.getDataDeCriacaoComent());
+
+            return ResponseEntity.status(HttpStatus.OK)
+            .body(comentarioRepository.save(comentEncont));
+            
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{idComent}")
+    public ResponseEntity<String> excluirComentario(@PathVariable("idComent") Long idComent) {
+
+        comentarioRepository.deleteById(idComent);
+        return ResponseEntity.status(HttpStatus.OK)
+        .body("Comentário excluído com sucesso!");
+    }
+
+
 
 
     
