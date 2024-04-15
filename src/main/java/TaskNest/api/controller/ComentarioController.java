f622d7a2063577cfb094d1708cfe1dcb62feca43
Fabiana <fabiana.lua@gmail.com>
2024-04-14 21:25:39 -0300

    Criação dos endpoints GET para exibir lista de comentários e buscar cometário pelo id

--- src/main/java/TaskNest/api/controller/ComentarioController.java
+++ src/main/java/TaskNest/api/controller/ComentarioController.java
@@ -1,8 +1,13 @@
 package TaskNest.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import TaskNest.api.model.Comentario;
 import TaskNest.api.repository.ComentarioRepository;
+import lombok.experimental.PackagePrivate;
 
 @RestController
 @RequestMapping("/comentario")
@@ -24,6 +30,20 @@ public class ComentarioController {
         .body(comentarioRepository.save(comentario));
     }
 
+    @GetMapping
+    public ResponseEntity<List<Comentario>> exibirListaDeComentarios() {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(comentarioRepository.findAll());
+    }
+
+    @GetMapping("/{idComent}")
+    public ResponseEntity<Optional<Comentario>> buscarComentarioPeloId(@PathVariable("idComent") Long idComent) {
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(comentarioRepository.findById(idComent));
+    }
+
     
+
+
     
 }
