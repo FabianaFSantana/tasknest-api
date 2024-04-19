7fd6264582413003b1f4f1e373c1319fab8fbd7d
Fabiana <fabiana.lua@gmail.com>
2024-04-19 09:24:14 -0300

    TAS-010 - Criação da classe EmailTarefaService

--- src/main/java/TaskNest/api/service/MensagemSmsService.java
+++ src/main/java/TaskNest/api/service/MensagemSmsService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.twilio.Twilio;
-import com.twilio.rest.accounts.v1.credential.PublicKey;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
